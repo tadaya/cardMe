15e812d14f2020a641c12b5d11ec6e8735fc4761
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-03-19 11:39:52 -0400

    Initial cardme Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cardme::Application.config.secret_key_base = '12007dfcadb39e54e70ed2536191bedab8e705f7535fdfb603013f69254aa13efbf41e03806400d6f661a1e9877ae9f0f78ccba4bf9e40c7b109a4923069d668'
