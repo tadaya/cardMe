1797339fa6a2db684d220cb8befe029232e51387
Sandy Yu <yu.sandy@live.com>
2014-03-20 11:00:25 -0400

    update conflicts

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-class CardsController < ActionController::Base
-  def index
-    @group = Group.new
-=======
-class CardsController < ApplicationController
 
-  def index
-    @user = current_user
-    @connections = @user.connections
->>>>>>> c56339f398a82aa682333cb7c29d09bdf4a023e5
-=======
 class CardsController < ActionController::Base
   def index
     @group = Group.new
     @user = current_user
     @connections = @user.connections
->>>>>>> afc3f15007e3b57fed380e42a31fc07db1dc5b0c
   end
 
 end
