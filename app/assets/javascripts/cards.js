7a2fb148a32095c4a5b6cd09ec30031f31264434
Tahira <tahira.adaya@gmail.com>
2014-03-24 19:07:14 -0400

    no changes

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -2,7 +2,12 @@ var allGroups;
 var allCards;
 var allConnections;
 
-<<<<<<< HEAD
+
+function Card(user, connection){
+
+}
+
+// CREATE NEW GROUPS
 function addGroups() {
   $('#group_form').on("submit", function(e){
     e.preventDefault();
@@ -19,7 +24,7 @@ function addGroups() {
   });
 }
 
-
+// GET ALL CONNECTIONS and MATCH WHICH ONES ARE USERS OWN
 function getConnections(){
   $.getJSON("/connections", function(response){
     allConnections = response;
@@ -32,67 +37,82 @@ function getConnections(){
   });
 }
 
+// MAKE CARDS
 function makeCards(i){
   $.getJSON("/cards/" + allConnections[i].card_id, function(cardFound) {
     var cards = $("<div class='card' id='" + cardFound.id + "' data-connection='" + allConnections[i].id + "'>");
     var cardmenu = $("<div class='cardmenu'></div>")
     var cardContainer = $("<div class='cardContainer'></div>")
     $(cards).appendTo("ul.connection-cards");
-    $("<li>" + "Email: " + cardFound.email + "</li>").appendTo(cards);
-    $("<li>" + "Phone Number: " + cardFound.phone_number + "</li>").appendTo(cards);
-    $("<li>" + "Organization: " + cardFound.organization + "</li>").appendTo(cards);
-    $("<li>" + "Position: " + cardFound.position + "</li>").appendTo(cards);
+    $("<li>" + cardFound.email + "</li>").appendTo(cards);
+    $("<li>" + cardFound.phone_number + "</li>").appendTo(cards);
+    $("<li>" + cardFound.organization + "</li>").appendTo(cards);
+    $("<li>" + cardFound.position + "</li>").appendTo(cards);
     $("<button class='add'> + </button>").appendTo(cardmenu).on("click", addCardToGroup);
-    $("<button class='arrow'> > </button>").appendTo(cardmenu).on("click", cardDashboard);
+    $("<button class='arrow'> > </button>").appendTo(cardmenu).on("click", console.log("Dashboard"));
     $(cardmenu).insertAfter(cards)
     $(cardmenu).appendTo(cardContainer)
     $(cards).appendTo(cardContainer)
     $(cardContainer).appendTo($(".connection-cards"))
   });
 }
-=======
->>>>>>> e53cce76c45098ba40d45cdd1b24f54f9d904441
 
-function Card(user, connection){
 
-<<<<<<< HEAD
+function Check(connection_id){
+  this.connection_id = connection_id
+  this.resonse = 
+        for(var i = 0; i < current_connections.length; i++){
+          if((current_connections[i].connection_id === parseInt(connection_id)) && (current_connections[i].group_id === allGroups[index].id)){
+            return true;
+          } else {
+            return false;
+          }
+        };
+  })};
+};
+
+// ADD CARD TO GROUP 
 function addCardToGroup(){
   $('#add-group').remove();
   $("ul.groups_popup").detach();
   $("<ul class='groups_popup'>").appendTo($(this).parent());
-
-  var connection_id = $(this).parent().parent().find(".card").attr("data-connection");
-  // Making ajax request to get all the groups
-  $.getJSON("/users/" + localStorage["user_id"] + "/groups", function(response){
+  groupsResponse =  $.getJSON("/users/" + localStorage["user_id"] + "/groups");
+  connectionsResponse = $.getJSON("/connections/" + connection_id + "/groupsconnections");
+  function(response){
     allGroups = response;
-    // Get all of the connections that are already grouped together
-    $.getJSON("/connections/" + connection_id + "/groupsconnections", function(response) {
-      var current_connections = response;
-
-      // Iterate through the groups and make a new li and checkbox for the user to make a choice
-      for(var i = 0; i < allGroups.length; i++) {
-        var checkbox = $("<input type='checkbox'>");
-
-        // Iterate through the table that has the information about which cards have already been connected to a specific group, and if they have been connected previously, check the checkbox true. Otherwise leave it blank.
-        for(var j = 0; j < current_connections.length; j++){
-          if((current_connections[j].connection_id === parseInt(connection_id)) && (current_connections[j].group_id === allGroups[i].id)){
-            $(checkbox).prop('checked', true);
-          }
-        }
-        // Append the li and the checkbox(including it's value) to the popup div called groups_popup
-        groupNames = $("<li id=" + allGroups[i].id + ">" + allGroups[i].group_name + "</li>").appendTo("ul.groups_popup").append(checkbox);
-        $(checkbox).on("change", selectGroup);
-      }
-    });
-    // This button closes the popup box and refreshes all of the connections 
-    $("<button>Add To Groups</button>").appendTo("ul.groups_popup").on("click", getConnections);
+    connectionsResponse = 
+    for(var i = 0; i < allGroups.length; i++) {
+      var connection_id = $(this).parent().parent().parent().parent().attr("data-connection");
+      checkbox = $("<input type ='checkbox'>");
+      check = new Check(connection_id);
+      checkbox.prop("checked", check.checked());
+      $("ul.groups_popup").append(checkbox);
+      $("<li id=" + allGroups[i].id + ">" + allGroups[i].group_name + "</li>").appendTo("ul.groups_popup").append(checkbox);
+      checkbox.on("change", selectGroup);
+    }
+    $("<button class='addGroupButton'>Add To Groups</button>").appendTo("ul.groups_popup").on("click", getConnections);
   });
 }
+
+getConnectionGroupInfo = function(index, connection_id, checkbox){
+    $.getJSON("/connections/" + connection_id + "/groupsconnections", function(response) {
+        current_connections = response;
+        for(var i = 0; i < current_connections.length; i++){
+          if((current_connections[i].connection_id === parseInt(connection_id)) && (current_connections[i].group_id === allGroups[index].id)){
+            checkbox.checked = true;
+          } else {
+            checkbox.checked = false;
+          }
+        }
+    });
+  };
+
+
 
 function selectGroup() {
   var group_id = $(this).parent().attr('id');
   var connection_id = $(this).parent().parent().parent().attr("data-connection");
-  // This posts the connection to whatever group is checked
+
   if(this.checked === true){
     $.ajax({
       url: "/groupsconnections",
@@ -100,7 +120,6 @@ function selectGroup() {
       type: "POST"
     });
   } else {
-    // This deletes the connection from a group
     $.ajax({
       url: "/groupsconnections",
       data: {connection: connection_id, group: group_id},
@@ -138,194 +157,22 @@ function cardDashboard(){
   $(this).parent().parent().find(".card").clone().appendTo(".showcard");
   $(".showcard .cardmenu").remove();
   cardId = $(".showcard .card").attr("id");
-  console.log(cardId);
   $(".articles li").remove();
   $.get("/card_dashboard/"+cardId, {card_id: cardId}, function(response){
       companySummary = response[0]["company_summary"];
+      // logo = response[0]["logo"];
       $("<div class=company_summary> Summary:" + companySummary + "</div>").appendTo(".showcard");
+      // $("<div class=company_logo><img src=" + logo + "/></div>").appendTo(".showcard");
       companyNews = response[0].news;
       for (var i = 0; i < 4; i++){
         newsResponse = companyNews[i];
-        $(".articles").append($("<a href=" + newsResponse["Url"] + "><li>" + newsResponse["Title"] + "</li></a>"));
+        $(".articles").append($("<a href=" + newsResponse["Url"] + "><li>" + newsResponse["Title"] + "</li></a>"))
       }
-    });
-}
+    })
+};
 
-
-cardDashboard();
+//cardDashboard();
 getConnections();
 showGroups();
 addGroups();
-=======
-}
-
-// // CREATE NEW GROUPS
-// function addGroups() {
-//   $('#group_form').on("submit", function(e){
-//     e.preventDefault();
-//     input = $('input#group_group_name');
-//     $.ajax({
-//       url: '/users/' + localStorage["user_id"] + '/groups',
-//       // localStorage["user_id"] is a key for a special localstorage hash
-//       // that can be found in application.html.erb DmitryJ
-//       type: 'POST',
-//       data: {group_name: input.val()}
-//     });
-//     input.val("");
-//   showGroups();
-//   });
-// }
-
-// // GET ALL CONNECTIONS and MATCH WHICH ONES ARE USERS OWN
-// function getConnections(){
-//   $.getJSON("/connections", function(response){
-//     allConnections = response;
-//     $("ul.connection-cards").empty();
-//     for(var i = 0; i < allConnections.length; i++){
-//       if(allConnections[i].user_id == localStorage["user_id"]) {
-//         makeCards(i);
-//       }
-//     }
-//   });
-// }
-
-// // MAKE CARDS
-// function makeCards(i){
-//   $.getJSON("/cards/" + allConnections[i].card_id, function(cardFound) {
-//     var cards = $("<div class='card' id='" + cardFound.id + "' data-connection='" + allConnections[i].id + "'>");
-//     var cardmenu = $("<div class='cardmenu'></div>")
-//     var cardContainer = $("<div class='cardContainer'></div>")
-//     $(cards).appendTo("ul.connection-cards");
-//     $("<li>" + cardFound.email + "</li>").appendTo(cards);
-//     $("<li>" + cardFound.phone_number + "</li>").appendTo(cards);
-//     $("<li>" + cardFound.organization + "</li>").appendTo(cards);
-//     $("<li>" + cardFound.position + "</li>").appendTo(cards);
-//     $("<button class='add'> + </button>").appendTo(cardmenu).on("click", addCardToGroup);
-//     $("<button class='arrow'> > </button>").appendTo(cardmenu).on("click", console.log("Dashboard"));
-//     $(cardmenu).insertAfter(cards)
-//     $(cardmenu).appendTo(cardContainer)
-//     $(cards).appendTo(cardContainer)
-//     $(cardContainer).appendTo($(".connection-cards"))
-//   });
-// }
-
-
-// function Check(connection_id){
-//   this.connection_id = connection_id
-//   this.resonse = 
-//         for(var i = 0; i < current_connections.length; i++){
-//           if((current_connections[i].connection_id === parseInt(connection_id)) && (current_connections[i].group_id === allGroups[index].id)){
-//             return true;
-//           } else {
-//             return false;
-//           }
-//         };
-//   })};
-// };
-
-// // ADD CARD TO GROUP 
-// function addCardToGroup(){
-//   $('#add-group').remove();
-//   $("ul.groups_popup").detach();
-//   $("<ul class='groups_popup'>").appendTo($(this).parent());
-//   groupsResponse =  $.getJSON("/users/" + localStorage["user_id"] + "/groups");
-//   connectionsResponse = $.getJSON("/connections/" + connection_id + "/groupsconnections");
-//   function(response){
-//     allGroups = response;
-//     connectionsResponse = 
-//     for(var i = 0; i < allGroups.length; i++) {
-//       var connection_id = $(this).parent().parent().parent().parent().attr("data-connection");
-//       checkbox = $("<input type ='checkbox'>");
-//       check = new Check(connection_id);
-//       checkbox.prop("checked", check.checked());
-//       $("ul.groups_popup").append(checkbox);
-//       $("<li id=" + allGroups[i].id + ">" + allGroups[i].group_name + "</li>").appendTo("ul.groups_popup").append(checkbox);
-//       checkbox.on("change", selectGroup);
-//     }
-//     $("<button class='addGroupButton'>Add To Groups</button>").appendTo("ul.groups_popup").on("click", getConnections);
-//   });
-// }
-
-// getConnectionGroupInfo = function(index, connection_id, checkbox){
-//     $.getJSON("/connections/" + connection_id + "/groupsconnections", function(response) {
-//         current_connections = response;
-//         for(var i = 0; i < current_connections.length; i++){
-//           if((current_connections[i].connection_id === parseInt(connection_id)) && (current_connections[i].group_id === allGroups[index].id)){
-//             checkbox.checked = true;
-//           } else {
-//             checkbox.checked = false;
-//           }
-//         }
-//     });
-//   };
-
-
-
-// function selectGroup() {
-//   var group_id = $(this).parent().attr('id');
-//   var connection_id = $(this).parent().parent().parent().attr("data-connection");
-
-//   if(this.checked === true){
-//     $.ajax({
-//       url: "/groupsconnections",
-//       data: {connection: connection_id, group: group_id},
-//       type: "POST"
-//     });
-//   } else {
-//     $.ajax({
-//       url: "/groupsconnections",
-//       data: {connection: connection_id, group: group_id},
-//       type: "DELETE"
-//     });
-//   }
-// }
-
-
-
-// function showGroups() {
-//   $.getJSON("/users/" + localStorage["user_id"] + "/groups", function(response) {
-//     allGroups = response;
-//     $("ul.groups").empty();
-//     for(var i = 0; i < allGroups.length; i++) {
-//       ($("<li>" + allGroups[i].group_name + "</li>").append("<span id="+ allGroups[i].id + ">" + ' X ' + "</span>")).appendTo("ul.groups");
-//     }// for loop ends
-//     $("span").on("click", function(e){
-//       var groupId = e.target.id;
-//       $.ajax({
-//         url: '/users/' + localStorage["user_id"] + '/groups/' + groupId,
-//         type: "DELETE",
-//         success: function(){
-//           e.target.parentElement.remove();
-//         }//ends removing element from dom
-//       });
-//       //ajax closed
-//     });// ends listener
-//   });// ends getJSON
-// }// ends showGroups
-
-
-// function cardDashboard(){
-//   $(".showcard div").remove();
-//   $(this).parent().parent().find(".card").clone().appendTo(".showcard");
-//   $(".showcard .cardmenu").remove();
-//   cardId = $(".showcard .card").attr("id");
-//   $(".articles li").remove();
-//   $.get("/card_dashboard/"+cardId, {card_id: cardId}, function(response){
-//       companySummary = response[0]["company_summary"];
-//       // logo = response[0]["logo"];
-//       $("<div class=company_summary> Summary:" + companySummary + "</div>").appendTo(".showcard");
-//       // $("<div class=company_logo><img src=" + logo + "/></div>").appendTo(".showcard");
-//       companyNews = response[0].news;
-//       for (var i = 0; i < 4; i++){
-//         newsResponse = companyNews[i];
-//         $(".articles").append($("<a href=" + newsResponse["Url"] + "><li>" + newsResponse["Title"] + "</li></a>"))
-//       }
-//     })
-// };
-
-// //cardDashboard();
-// getConnections();
-// showGroups();
-// addGroups();
->>>>>>> e53cce76c45098ba40d45cdd1b24f54f9d904441
 
