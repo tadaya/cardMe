a8ff10741ffc5d11c21cdf399e12e728f83061b2
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-03-24 17:13:30 -0400

    checkboxes persist from database

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -46,25 +46,40 @@ function makeCards(i){
 function addCardToGroup(){
   $('ul.groups_popup').remove();
   $("<ul class='groups_popup'>").appendTo($(this).parent());
+
+  var connection_id = ($(this).parent().attr("data-connection"));
+  // Making ajax request to get all the groups
   $.getJSON("/users/" + localStorage["user_id"] + "/groups", function(response){
-  allGroups = response;
+    allGroups = response;
+    // Get all of the connections that are already grouped together
+    $.getJSON("/connections/" + connection_id + "/groupsconnections", function(response) {
+      var current_connections = response;
 
-    for(var i = 0; i < allGroups.length; i++) {
-      var connection_id = $(this).parent().parent().parent().parent().attr("data-connection");
-      checkbox = $("<input type='checkbox'>");
-      
-      $("<li id=" + allGroups[i].id + ">" + allGroups[i].group_name + "</li>").appendTo("ul.groups_popup").append(checkbox);
-      checkbox.on("change", selectGroup);
-    }
+      // Iterate through the groups and make a new li and checkbox for the user to make a choice
+      for(var i = 0; i < allGroups.length; i++) {
+        var checkbox = $("<input type='checkbox'>");
 
-    $("<button>Save</button>").appendTo("ul.groups_popup").on("click", getConnections);
+        // Iterate through the table that has the information about which cards have already been connected to a specific group, and if they have been connected previously, check the checkbox true. Otherwise leave it blank.
+        for(var j = 0; j < current_connections.length; j++){
+          if((current_connections[j].connection_id === parseInt(connection_id)) && (current_connections[j].group_id === allGroups[i].id)){
+            $(checkbox).prop('checked', true);
+          }
+        }
+        // Append the li and the checkbox(including it's value) to the popup div called groups_popup
+        groupNames = $("<li id=" + allGroups[i].id + ">" + allGroups[i].group_name + "</li>").appendTo("ul.groups_popup").append(checkbox);
+        $(checkbox).on("change", selectGroup);
+      }
+    });
+    // This button closes the popup box and refreshes all of the connections 
+    $("<button>Add To Groups</button>").appendTo("ul.groups_popup").on("click", getConnections);
+  
   });
 }
 
 function selectGroup() {
   var group_id = $(this).parent().attr('id');
   var connection_id = $(this).parent().parent().parent().attr("data-connection");
-
+  // This posts the connection to whatever group is checked
   if(this.checked === true){
     $.ajax({
       url: "/groupsconnections",
@@ -72,6 +87,7 @@ function selectGroup() {
       type: "POST"
     });
   } else {
+    // This deletes the connection from a group
     $.ajax({
       url: "/groupsconnections",
       data: {connection: connection_id, group: group_id},
@@ -104,20 +120,20 @@ function showGroups() {
 }// ends showGroups
 
 
-function newStories(){
-$(".connection-cards").on("click","div", function(){
-  $(".showcard div").remove();
-  $(this).clone().appendTo(".showcard");
-  cardId = $(".showcard .card").attr("id");
-  $(".articles li").remove();
-  news = $.get("/card_news/"+cardId, {card_id: cardId}, function(){
-      for (var i = 0; i < 4; i++){
-        newsResponse = news.responseJSON[i]
-        $(".articles").append($("<a href=" + newsResponse["Url"] + "><li>" + newsResponse["Title"] + "</li></a>"))
-      }
-    })
-  });
-};
+// function newStories(){
+// $(".connection-cards").on("click","div", function(){
+//   $(".showcard div").remove();
+//   $(this).clone().appendTo(".showcard");
+//   cardId = $(".showcard .card").attr("id");
+//   $(".articles li").remove();
+//   news = $.get("/card_news/"+cardId, {card_id: cardId}, function(){
+//       for (var i = 0; i < 4; i++){
+//         newsResponse = news.responseJSON[i]
+//         $(".articles").append($("<a href=" + newsResponse["Url"] + "><li>" + newsResponse["Title"] + "</li></a>"))
+//       }
+//     })
+//   });
+// };
 
 // newStories();
 getConnections();
